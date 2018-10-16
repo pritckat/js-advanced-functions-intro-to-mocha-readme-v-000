8f9f8a663d7ed15ac53b0d90531f689085b7630e
Katrina Pritchard <pritckat@gmail.com>
2018-10-16 01:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ function sayHeyFriend(name) {
 }
 
 function favoriteIceCream(flavor) {
-  return `I love ${flavor}.`
+  return `I love ${flavor}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
 }
